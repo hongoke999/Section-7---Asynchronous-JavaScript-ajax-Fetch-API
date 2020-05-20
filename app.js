05e1ecd74157ea9914b87a83e6f07faeeb5c3351
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-20 17:21:26 +0300

    Data From an External API - Chuck Norris Project

--- app.js
+++ app.js
@@ -1,66 +1,31 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
-document.getElementById('button1').addEventListener('click', loadCustomer);
-
-//  Load a Single Customer
-function loadCustomer(e) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'customer.json', true);
-
-    xhr.onload = function() {
-        if(this.status === 200) {
-            // console.log(this.responseText);
-
-            const customer = JSON.parse(this.responseText);
-
-            const output = `
-                <ul>
-                    <li>ID: ${customer.id}</li>
-                    <li>Name: ${customer.name}</li>
-                    <li>Company: ${customer.company}</li>
-                    <li>Phone: ${customer.phone}</li>
-                </ul>
-            `;
-
-            document.getElementById('customer').innerHTML = output;
-        }
-    }
-
-    xhr.send();
-}
-
-
-
-document.getElementById('button2').addEventListener('click', loadCustomers);
-
-//  Load Customers
-function loadCustomers(e) {
+function getJokes(e) {
+    const number = document.querySelector('input[type="number"]').value;
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'customers.json', true);
-    
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
     xhr.onload = function() {
         if(this.status === 200) {
-            const customers = JSON.parse(this.responseText);
-
+            const response = JSON.parse(this.responseText);
+            
             let output = '';
 
-            customers.forEach(function(customer){
-                output += `
-                    <ul>
-                        <li>ID: ${customer.id}</li>
-                        <li>Name: ${customer.name}</li>
-                        <li>Company: ${customer.company}</li>
-                        <li>Phone: ${customer.phone}</li>
-                    </ul>
-                `;
-                
-                document.getElementById('customers').innerHTML = output;
-            });            
+            if(response.type === 'success') {
+                response.value.forEach(function(joke) {
+                    output += `<li>${joke.joke}</li>`;
+                });
+            } else {
+                output += `<li>Something went wrong!</li>`;
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
 
     xhr.send();
+
     e.preventDefault();
-} 
+}
