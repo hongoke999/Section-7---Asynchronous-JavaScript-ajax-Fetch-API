a1c1617728c4b2b6d3e73b6ed8798ee0c3c2784a
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-22 09:34:14 +0300

    Custom HTTP Library (Ajax With Callbacks) - Part 2

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
 const http = new easyHTTP;
 
 //  Get Posts
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+/* const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
   if(err) {
     console.log(err);
   } else {
     console.log(posts);
   }
-});
+}); */
 
+//  Get Single Post
+/* const posts = http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+}); */
+
+
+//  Create Data
+const data = {
+  title: 'Custome post',
+  body: 'This is a custom post'
+};
+
+//  Create Post
+/* http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+}); */
+
+//  Update Post
+/* http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+}); */
+
+//  Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1',  
+function (err,response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
+    }
+  }
+);
