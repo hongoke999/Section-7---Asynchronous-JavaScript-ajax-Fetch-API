5a77a0b75ccefc25b89ab7ea2ae2e1981d0d1a5a
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-26 17:40:36 +0300

    Custom HTTP Library (Fetch With Promises) - Part 3

--- app.js
+++ app.js
@@ -1,95 +1,29 @@
-document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJson);
-document.getElementById('button3').addEventListener('click', getExternal);
+const http = new EasyHTTP;
 
+//  Get Users
+/* const users = http.get('https://jsonplaceholder.typicode.com/users')
+    .then(data => console.log(data))
+    .catch(err => console.log(err)); */
 
-/* // Get local text file data
-function getText() {
-  fetch('test.txt')
-    .then(function(res){
-      return res.text();
-    })
-    .then(function(data){
-      console.log(data);
-      document.getElementById('output').innerHTML = data;
-    })
-    .catch(function(err){
-      console.log(err);
-    });
+//  User Data
+const data = {
+    name: 'John',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
 }
 
-// Get local json data
-function getJson() {
-  fetch('posts.json')
-    .then(function(res){
-      return res.json();
-    })
-    .then(function(data){
-      console.log(data);
-      let output = '';
-      data.forEach(function(post) {
-        output += `<li>${post.title}</li>`;
-      });
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch(function(err){
-      console.log(err);
-    });
-}
+//  Create User
+/* http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err)); */
 
-// Get from external API
-function getExternal() {
-  fetch('https://api.github.com/users')
-    .then(function(res){
-      return res.json();
-    })
-    .then(function(data){
-      console.log(data);
-      let output = '';
-      data.forEach(function(user) {
-        output += `<li>${user.login}</li>`;
-      });
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch(function(err){
-      console.log(err);
-    });
-} */
+//  Update User
+/* http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err)); */
 
-//  With Arrow function
-// Get local text file data
-function getText() {
-  fetch('test.txt')
-    .then(res => res.text())
-    .then(data => {
-      console.log(data);
-      document.getElementById('output').innerHTML = data;
-    })
+//  Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
     .catch(err => console.log(err));
-}
 
-// Get local json data
-function getJson() {
-  fetch('posts.json')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        let output = '';
-        data.forEach(post => output += `<li>${post.title}</li>`);
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch(err => console.log(err));
-}
-
-// Get from external API
-function getExternal() {
-  fetch('https://api.github.com/users')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      let output = '';
-      data.forEach(user => output += `<li>${user.login}</li>`);
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch(err => console.log(err));
-}
